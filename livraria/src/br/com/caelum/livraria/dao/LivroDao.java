e7d19738657b563541531d714e338f237e6dc251
Andre Chaves <andrechavesg@gmail.com>
2018-03-30 17:09:19 -0300

    capitulo3

--- livraria/src/br/com/caelum/livraria/dao/LivroDao.java
+++ livraria/src/br/com/caelum/livraria/dao/LivroDao.java
@@ -4,20 +4,22 @@ import java.util.List;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import br.com.caelum.livraria.modelo.Livro;
 
 @Stateless
 public class LivroDao {
-	@Inject
-	private Banco banco;
-	
+	@PersistenceContext
+	private EntityManager manager;
+
 	public void salva(Livro livro) {
-		banco.save(livro);
+		manager.persist(livro);
 	}
-	
+
 	public List<Livro> todosLivros() {
-		return banco.listaLivros();
+		return manager.createQuery("select l from Livro l", Livro.class).getResultList();
 	}
-	
+
 }
--- livraria/src/br/com/caelum/livraria/dao/UsuarioDao.java
+++ livraria/src/br/com/caelum/livraria/dao/UsuarioDao.java
@@ -2,16 +2,21 @@ package br.com.caelum.livraria.dao;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import br.com.caelum.livraria.modelo.Usuario;
 
 @Stateless
 public class UsuarioDao {
-	@Inject
-	private Banco banco;
+    @PersistenceContext
+	private EntityManager manager;
 
 	public Usuario buscaPeloLogin(String login) {
-		return this.banco.buscaPeloNome(login);
+
+		Usuario usuario = (Usuario) this.manager.createQuery("select u from Usuario u where u.login=:pLogin")
+				.setParameter("pLogin", login).getSingleResult();
+		return usuario;
 	}
-	
+
 }
