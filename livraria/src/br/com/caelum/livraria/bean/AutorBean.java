04fd40f32e9acbb7a47bb598670b0056a7ffb59a
Andre Chaves <andrechavesg@gmail.com>
2018-03-30 17:34:45 -0300

    capitulo4

--- livraria/src/br/com/caelum/livraria/bean/AutorBean.java
+++ livraria/src/br/com/caelum/livraria/bean/AutorBean.java
@@ -13,18 +13,18 @@ public class AutorBean {
 	
 	private Autor autor = new Autor();
 	@Inject
-	private AutorDao dao;
+	private AutorService service;
 	
 	public Autor getAutor() {
 		return autor;
 	}
 	
 	public void cadastra() {
-		this.dao.salva(autor);
+		this.service.adiciona(autor);
 		this.autor = new Autor();
 	}
 	
 	public List<Autor> getAutores() {
-		return this.dao.todosAutores();
+		return this.service.todosAutores();
 	}
 }
--- livraria/src/br/com/caelum/livraria/dao/AutorDao.java
+++ livraria/src/br/com/caelum/livraria/dao/AutorDao.java
@@ -4,13 +4,17 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
-import javax.inject.Inject;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.ejb.TransactionManagementType;
+import javax.ejb.TransactionManagement;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import br.com.caelum.livraria.modelo.Autor;
 
 @Stateless
+@TransactionManagement(TransactionManagementType.CONTAINER)
 public class AutorDao {
 	@PersistenceContext
 	private EntityManager manager;
@@ -20,6 +24,7 @@ public class AutorDao {
 	    System.out.println("[INFO] AutorDao foi criado.");
 	}
 	
+    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
 	public void salva(Autor autor) {
 	    System.out.println("[INFO] Salvando o Autor " + autor.getNome());
 
