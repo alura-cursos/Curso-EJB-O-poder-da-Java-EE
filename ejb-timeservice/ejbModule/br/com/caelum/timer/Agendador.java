786070a27964dffaf2077b5d4aaff7170848c367
André Chaves <andrechavesg@gmail.com>
2018-05-21 15:07:30 -0300

    capitulo8

---
+++ ejb-timeservice/ejbModule/br/com/caelum/timer/Agendador.java
@@ -0,0 +1,14 @@
+package br.com.caelum.timer;
+
+import javax.ejb.Schedule;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+
+@Singleton
+@Startup
+public class Agendador {
+	@Schedule(hour = "*", minute = "*", second = "*/10", persistent = false)
+	void agendado() {
+		System.out.println("verificando serviço externo periodicamente");
+	}
+}
