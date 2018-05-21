27dc39081a0c40a8a1933d3e123fcf1ca25c18e6
André Chaves <andrechavesg@gmail.com>
2018-05-21 14:56:52 -0300

    capitulo7

--- livraria/src/br/com/caelum/livraria/dao/LivroDao.java
+++ livraria/src/br/com/caelum/livraria/dao/LivroDao.java
@@ -3,9 +3,9 @@ package br.com.caelum.livraria.dao;
 import java.util.List;
 
 import javax.ejb.Stateless;
-import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import br.com.caelum.livraria.modelo.Livro;
 
@@ -22,4 +22,12 @@ public class LivroDao {
 		return manager.createQuery("select l from Livro l", Livro.class).getResultList();
 	}
 
+	public List<Livro> livrosPeloNome(String nome) {
+
+		TypedQuery<Livro> query = this.manager.createQuery("select l from Livro l " + "where l.titulo like :pTitulo",
+				Livro.class);
+		query.setParameter("pTitulo", "%" + nome + "%");
+
+		return query.getResultList();
+	}
 }
